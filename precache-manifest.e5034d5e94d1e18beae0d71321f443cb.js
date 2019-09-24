fc8f0427897be57bc703e73bc9d63a7043a389ff
eugeneklochkov <fop.klochkov@gmail.com>
2019-09-24 08:17:13 +0300

    Updates

---
+++ precache-manifest.e5034d5e94d1e18beae0d71321f443cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15564e0b0c9cbe667eccfbd1d01668bd",
+    "url": "/Countries/index.html"
+  },
+  {
+    "revision": "2242185be3933c7bba08",
+    "url": "/Countries/static/css/main.22a76f4e.chunk.css"
+  },
+  {
+    "revision": "8514276956e625dc914f",
+    "url": "/Countries/static/js/2.80881208.chunk.js"
+  },
+  {
+    "revision": "2242185be3933c7bba08",
+    "url": "/Countries/static/js/main.0f8ade29.chunk.js"
+  },
+  {
+    "revision": "2b7203b6c9ec7165f276",
+    "url": "/Countries/static/js/runtime~main.87fbf5e1.js"
+  }
+]);
